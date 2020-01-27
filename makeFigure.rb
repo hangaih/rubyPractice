d3e19bc39475e3440bc784189bf8c444ce0b4667
hangaih <hangai.acc@gmail.com>
2020-01-27 18:21:20 +0900

    test

--- makeFigure.rb
+++ makeFigure.rb
@@ -34,3 +34,24 @@ for i in 0..5
   puts s
 end
 
+puts '------------'
+
+
+
+i = 0
+while i < 5 do
+  s =''
+  g = 0
+  while g < 5-i do
+    s += ' '
+    g += 1
+  end
+  s += '*'
+  h = 0
+  while h < i do
+    s += '**'
+    h += 1
+  end
+  i += 1
+  puts s
+end
